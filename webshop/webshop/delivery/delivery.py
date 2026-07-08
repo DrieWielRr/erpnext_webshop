512b28c239c21f70019d9b38ead0f9c16ab19f54
DrieWielRr <randysteven88@hotmail.com>
2026-07-08 12:44:06 +0200

    Updated to allow delivery date update without the deliver checked (for pickup purposes)

--- webshop/webshop/delivery/delivery.py
+++ webshop/webshop/delivery/delivery.py
@@ -335,21 +335,18 @@ def update_shipping(quotation, include_shipping, delivery_date=None):
 
     doc.custom_include_shipping = cint(include_shipping)
 
-    if doc.custom_include_shipping:
-        if not delivery_date:
-            frappe.throw(_("Please select a delivery date."))
+    if not delivery_date:
+        frappe.throw(_("Please select a delivery date."))
 
-        selected_date = getdate(delivery_date)
-        minimum_date = add_days(getdate(), 1)
+    selected_date = getdate(delivery_date)
+    minimum_date = add_days(getdate(), 1)
 
-        if selected_date < minimum_date:
-            frappe.throw(
-                _("The earliest available delivery date is {0}.").format(minimum_date)
-            )
+    if selected_date < minimum_date:
+        frappe.throw(
+            _("The earliest available delivery date is {0}.").format(minimum_date)
+        )
 
-        doc.custom_delivery_date = selected_date
-    else:
-        doc.custom_delivery_date = None
+    doc.custom_delivery_date = selected_date
 
     log(f"Quotation {doc.name}: custom_include_shipping={doc.custom_include_shipping}, delivery-date: {doc.custom_delivery_date}")
 
