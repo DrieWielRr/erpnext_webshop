2149dd4db17c7c862a1ea4f166563c1a7848447f
DrieWielRr <randysteven88@hotmail.com>
2026-08-11 21:18:32 +0200

    coupon recalculation fix

--- webshop/webshop/shopping_cart/cart.py
+++ webshop/webshop/shopping_cart/cart.py
@@ -1232,37 +1232,40 @@ def show_terms(doc):
 
 @frappe.whitelist(allow_guest=True)
 def apply_coupon_code(applied_code, applied_referral_sales_partner):
-	quotation = True
+    quotation = True
 
-	if not applied_code:
-		frappe.throw(_("Please enter a coupon code"))
+    if not applied_code:
+        frappe.throw(_("Please enter a coupon code"))
 
-	coupon_list = frappe.get_all("Coupon Code", filters={"coupon_code": applied_code})
-	if not coupon_list:
-		frappe.throw(_("Please enter a valid coupon code"))
+    coupon_list = frappe.get_all("Coupon Code", filters={"coupon_code": applied_code})
+    if not coupon_list:
+        frappe.throw(_("Please enter a valid coupon code"))
 
-	coupon_name = coupon_list[0].name
+    coupon_name = coupon_list[0].name
 
-	from erpnext.accounts.doctype.pricing_rule.utils import validate_coupon_code
+    from erpnext.accounts.doctype.pricing_rule.utils import validate_coupon_code
 
-	validate_coupon_code(coupon_name)
-	quotation = _get_cart_quotation()
-	quotation.ignore_pricing_rule = 0
-	quotation.coupon_code = coupon_name
-	quotation.flags.ignore_permissions = True
-	quotation.save()
+    validate_coupon_code(coupon_name)
+    quotation = _get_cart_quotation()
+    quotation.ignore_pricing_rule = 0
+    quotation.coupon_code = coupon_name
 
-	if applied_referral_sales_partner:
-		sales_partner_list = frappe.get_all(
-			"Sales Partner", filters={"referral_code": applied_referral_sales_partner}
-		)
-		if sales_partner_list:
-			sales_partner_name = sales_partner_list[0].name
-			quotation.referral_sales_partner = sales_partner_name
-			quotation.flags.ignore_permissions = True
-			quotation.save()
+    if applied_referral_sales_partner:
+        sales_partner_list = frappe.get_all(
+            "Sales Partner", filters={"referral_code": applied_referral_sales_partner}
+        )
+        if sales_partner_list:
+            sales_partner_name = sales_partner_list[0].name
+            quotation.referral_sales_partner = sales_partner_name
 
-	return quotation
+    # RECALCULATE
+    quotation.apply_pricing_rule()
+    quotation.calculate_taxes_and_totals()
+
+    quotation.flags.ignore_permissions = True
+    quotation.save()
+
+    return quotation
 
  
 @frappe.whitelist(allow_guest=True)
